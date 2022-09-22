d1c0bb61dc7c8062dda8f4c914bcc0ec6848431e
ChenGang <chengang21@hotmail.com>
2022-09-22 11:38:55 +0800

    Update ERC721A.sol

--- src/ERC721A/ERC721A.sol
+++ src/ERC721A/ERC721A.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.8.16;
 
-import "IERC721.sol";
-import "IERC721Receiver.sol";
-import "IERC721Metadata.sol";
-import "IERC721Enumerable.sol";
-import "Address.sol";
-import "Context.sol";
-import "Strings.sol";
-import "ERC165.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC721/IERC721Receiver.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/utils/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/utils/Strings.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.6/contracts/utils/introspection/ERC165.sol";
 
 /**
  * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
@@ -75,7 +75,7 @@ contract ERC721A is
         string memory symbol_,
         uint256 maxBatchSize_,
         uint256 collectionSize_
-    ) public {
+    ) {
         require(
             collectionSize_ > 0,
             "ERC721A: collection must have a nonzero supply"
